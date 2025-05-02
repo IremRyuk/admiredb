c238a28f86203a06a885dd792916ede372cbc5f1
IremRyuk <IremRyuk>
2025-05-02 18:35:43 +0400

    change storage upload limit to 50MB

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongoose.connect(process.env.LINK)
 
 
 // Routes
-app.use(express.json())
+app.use(express.json({limit: '50mb'}))
 app.use(cors())
 app.use('/users',users)
 app.use('/usermenu',createJob)
